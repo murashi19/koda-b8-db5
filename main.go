28d88962b0c49f2e0ec96afec629376fdb879e3f
Muhamad Rafli Shidiq <muhamadraflishidiq@gmail.com>
2026-07-15 17:59:23 +0700

    feat: add all feature

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"minitask-db5/feats"
 	"minitask-db5/lib"
-	"minitask-db5/models"
+	"minitask-db5/utils"
+	"os"
 	"strconv"
+	"time"
 )
 
 func main() {
@@ -16,38 +19,43 @@ func main() {
 	}
 
 	fmt.Println("Berhasil terhubung ke database!")
+	done := make(chan struct{})
+	go utils.Loading(done, "Data is being processed...")
 
-	contacts, _ := models.GetAllContact(db)
-	if err != nil {
-		fmt.Printf("Gagal mengambil data kontak: %v", err)
-	}
+	time.Sleep(2 * time.Second)
+	close(done)
 
-	fmt.Println("----- Welcome to Contact List App -----")
-	fmt.Println("\n1. List Contacts")
-	fmt.Println("2. Add Contact")
-	fmt.Println("3. Update Contact")
-	fmt.Println("4. Delete Contact")
+	for {
+		utils.ClearScreen()
+		fmt.Println("----- Welcome to Contact List App -----")
+		fmt.Println("\n1. List Contacts")
+		fmt.Println("2. Add Contact")
+		fmt.Println("3. Update Contact")
+		fmt.Println("4. Delete Contact")
+		fmt.Println("\n0. Exit")
 
-	choose, err := strconv.Atoi(lib.Input("\nChoose Menu: "))
-	if err != nil {
-		fmt.Println("Invalid Choose Menu, Please Enter a Number")
-	}
-
-	switch choose {
-	case 1:
-		lib.ClearScreen()
-		fmt.Println("\n--- DAFTAR KONTAK ---")
-		if len(contacts) == 0 {
-			fmt.Println("Tidak ada data kontak.")
-			lib.EnterBack()
-		} else {
-			for _, c := range contacts {
-				fmt.Printf(" ID: %d\n Nama: %s\n Email: %s\n Telepon: %s\n", c.Id, c.Name, c.Email, c.Phone_number)
-			}
-			lib.EnterBack()
+		choose, err := strconv.Atoi(utils.Input("\nChoose Menu: "))
+		if err != nil {
+			fmt.Println("Invalid Choose Menu, Please Enter a Number")
+			time.Sleep(1 * time.Second)
+			continue
 		}
-	case 2:
 
+		switch choose {
+		case 1:
+			feats.ListContacts(db)
+		case 2:
+			feats.AddContact(db)
+		case 3:
+			feats.EditContact(db)
+		case 4:
+			feats.DeletedContact(db)
+		case 0:
+			os.Exit(0)
+		default:
+			fmt.Println("Please fill according to the menu")
+			time.Sleep(1 * time.Second)
+		}
 	}
 
 }
--- models/contacts.go
+++ models/contacts.go
@@ -35,15 +35,54 @@ func GetAllContact(conn *pgx.Conn) ([]Contact, error) {
 }
 
 func CreateContact(data Contact, conn *pgx.Conn) (Contact, error) {
-	rows, _ := conn.Query(context.Background(), `
+	rows, err := conn.Query(context.Background(), `
 		INSERT INTO contacts (name, email, phone_number) VALUES
-		('$1', '$2', '$3')
+		($1, $2, $3)
 		RETURNING id, name, email, phone_number, created_at, updated_at
-	`, data.Id, data.Name, data.Email, data.Phone_number, data.CreatedAt, data.UpdatedAt)
-
+	`, data.Name, data.Email, data.Phone_number)
+	if err != nil {
+		fmt.Println("Failed Insert Query")
+	}
 	contact, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[Contact])
 	if err != nil {
 		fmt.Println("Failed Colleting Data Rows")
 	}
 	return contact, nil
+}
+
+func UpdateContact(data Contact, conn *pgx.Conn) (Contact, error) {
+	rows, err := conn.Query(context.Background(), `
+		UPDATE contacts SET
+		name = $1,
+		email = $2,
+		phone_number = $3,
+		updated_at = NOW()
+		WHERE id = $4
+		RETURNING id, name, email, phone_number, created_at, updated_at
+	`, data.Name, data.Email, data.Phone_number, data.Id)
+	if err != nil {
+		fmt.Println("Failed Update Query")
+	}
+	contact, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[Contact])
+	if err != nil {
+		fmt.Println("Failed Colleting Data Rows")
+	}
+	return contact, nil
+}
+
+func DeleteContact(id int, conn *pgx.Conn) error {
+	commandTag, err := conn.Exec(context.Background(), `
+		DELETE FROM contacts
+		WHERE id = $1
+	`, id)
+	if err != nil {
+		fmt.Println("Failed Delete Query")
+	}
+	if commandTag.RowsAffected() == 0 {
+		return fmt.Errorf("Contact not found")
+	}
+	if err != nil {
+		fmt.Println("Failed Colleting Data Rows")
+	}
+	return nil
 }
